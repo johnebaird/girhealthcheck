b3fd1f8b4c1bc34cf2a0d73354b035d5e0fbf889
johnebaird <96591514+johnebaird@users.noreply.github.com>
2021-12-23 14:43:05 -0400

    Update girhealthcheck.py
    added webdav check

--- girhealthcheck.py
+++ girhealthcheck.py
@@ -1,6 +1,7 @@
 import re
 import json
 import requests
+from requests.auth import HTTPBasicAuth
 import time
 
 # comma seperate values of RWS hosts to check
@@ -11,21 +12,31 @@ RWS_URI = "/api/v2/diagnostics/version"
 ES_HOSTS = "http://192.168.45.81:9200,http://192.168.45.83:9200"
 ES_URI = "/_cluster/health"
 
+#command seperated values of WebDAV hosts to query
+WEBDAV_HOSTS = "http://192.168.45.81"
+WEBDAV_URI = "/recordings/"
+WEBDAV_USERNAME = "webdav"
+WEBDAV_PASSWORD = "webdav"
+
 #update interval in seconds between checks
 UPDATE = 10
 
-
 # Take a host and a URI and return appropriate HTTP status code and successful data from query or error response
-def checkURL(host, uri):
+def checkURL(host, uri, username, password):
     
     try:
         query = host + uri
         data = ""
-        data = requests.get(query)
+        
+        # check if we need to define a username and if so pass it
+        if username:
+            data = requests.get(query, auth=HTTPBasicAuth(username, password))
+        else:
+            data = requests.get(query)
         
                         
-    except:
-        return 0, "timeout"
+    except Exception as e:
+        return 0, str(e)
         
     else: 
         return data.status_code, data.text
@@ -34,19 +45,19 @@ def checkURL(host, uri):
 # check invidual RWS and print status
 def checkRWS(host, uri):
     
-    rwsStatus,rwsData = checkURL(host, RWS_URI)
+    rwsStatus,rwsData = checkURL(host, uri, "", "")
         
     if rwsStatus == 200:
         print ("    RWS " + host + ": UP")
     else:
         print ("    RWS " + host + ": DOWN  -   ", end='') 
-        print ("HTTP Status: " +str(rwsStatus) + " data: " + str(rwsData) )
+        print ("HTTP Status: " +str(rwsStatus) + " error: " + str(rwsData) )
             
 
 # check invididual ES and print status
 def checkES(host, uri):
 
-    esStatus, esData = checkURL(host, ES_URI)
+    esStatus, esData = checkURL(host, uri, "", "")
     
     if esStatus == 200:
         print ("    ES " + host + ": UP")
@@ -55,25 +66,38 @@ def checkES(host, uri):
         print ("HTTP Status: " +str(esStatus) + " data: " + str(esData) )
         
         
+def checkWebdav(host, uri, username, password):
+
+    webdavStatus, webdavData = checkURL(host, uri, username, password)
+    
+    ## Webdav returns 302 to /recordings and 200 to /recordings/ but both are successful requests
+    if webdavStatus == 200 or webdavStatus == 302:
+        print ("    WebDAV " + host + ": UP")
+    else:
+        print ("    WebDaV " + host + ": DOWN  -   ", end='') 
+        print ("HTTP Status: " +str(webdavStatus) + " data: " + str(webdavData) )
+
             
 while (1):
 
     print("Host status:")
     
     # check each RWS host
-    rwshosts = RWS_HOSTS.split(",")
-    
-    for host in rwshosts:
+    rwsHosts = RWS_HOSTS.split(",")
+    for host in rwsHosts:
         checkRWS(host, RWS_URI)
         
     
     ## check each ES host
-    eshosts = ES_HOSTS.split(",")
-    
-    for host in eshosts:
+    esHosts = ES_HOSTS.split(",")
+    for host in esHosts:
        checkES(host, ES_URI)
                 
-      
+    
+    ## check each WebDAV host
+    webdavHosts = WEBDAV_HOSTS.split(",")
+    for host in webdavHosts:
+        checkWebdav(host, WEBDAV_URI, WEBDAV_USERNAME, WEBDAV_PASSWORD)
       
     #time.sleep(UPDATE)
     break
