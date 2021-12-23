35c68353ba5ae84366fcdbab0c8ac864152b7e4b
johnebaird <96591514+johnebaird@users.noreply.github.com>
2021-12-23 16:38:35 -0400

    Update girhealthcheck.py
    added RPS

--- girhealthcheck.py
+++ girhealthcheck.py
@@ -4,24 +4,28 @@ import requests
 from requests.auth import HTTPBasicAuth
 import time
 
-# comma seperate values of RWS hosts to check
+# comma seperate values of RWS hosts 
 RWS_HOSTS = "http://192.168.45.81:8080"
 RWS_URI = "/api/v2/diagnostics/version"
 
-# comma seperated values of ES hosts to check
+# comma seperated values of ES hosts 
 ES_HOSTS = "http://192.168.45.81:9200,http://192.168.45.83:9200"
 ES_URI = "/_cluster/health"
 
-#command seperated values of WebDAV hosts to query
+#command seperated values of WebDAV 
 WEBDAV_HOSTS = "http://192.168.45.81"
 WEBDAV_URI = "/recordings/"
 WEBDAV_USERNAME = "webdav"
 WEBDAV_PASSWORD = "webdav"
 
+#comma seperate values of RPS hosts
+RPS_HOSTS = "http://192.168.45.81:8889"
+RPS_URI = "/api/status/"
+
 #update interval in seconds between checks
 UPDATE = 10
 
-# Take a host and a URI and return appropriate HTTP status code and successful data from query or error response
+# Take a host and a URI and return appropriate HTTP status code and successful json or error messages
 def checkURL(host, uri, username, password):
     
     try:
@@ -77,6 +81,18 @@ def checkWebdav(host, uri, username, password):
         print ("    WebDaV " + host + ": DOWN  -   ", end='') 
         print ("HTTP Status: " +str(webdavStatus) + " data: " + str(webdavData) )
 
+
+def checkRPS(host, uri):
+    
+    rpsStatus,rpsData = checkURL(host, uri, "", "")
+        
+    if rpsStatus == 200:
+        print ("    RPS " + host + ": UP")
+    else:
+        print ("    RPS " + host + ": DOWN  -   ", end='') 
+        print ("HTTP Status: " +str(rpsStatus) + " error: " + str(rpsData) )
+        
+        
             
 while (1):
 
@@ -98,7 +114,11 @@ while (1):
     webdavHosts = WEBDAV_HOSTS.split(",")
     for host in webdavHosts:
         checkWebdav(host, WEBDAV_URI, WEBDAV_USERNAME, WEBDAV_PASSWORD)
-      
+    
+    rpsHosts = RPS_HOSTS.split(",")
+    for host in rpsHosts:
+        checkRPS(host, RPS_URI)
+        
     #time.sleep(UPDATE)
     break
      
